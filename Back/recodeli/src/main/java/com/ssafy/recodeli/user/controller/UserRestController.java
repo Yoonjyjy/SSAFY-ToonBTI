50df84563d3de1f99fb4bb7bc44bc0883976b26c
전주영[서울_3반_A302]팀원 <wjswndud53@gmail.com>
2023-03-16 15:52:03 +0900

    feat: 로그인 DTO변경, 안 쓰는 코드 제거

--- Back/recodeli/src/main/java/com/ssafy/recodeli/user/controller/UserRestController.java
+++ Back/recodeli/src/main/java/com/ssafy/recodeli/user/controller/UserRestController.java
@@ -3,41 +3,24 @@ package com.ssafy.recodeli.user.controller;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.mail.MessagingException;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import com.ssafy.recodeli.user.model.dto.EmailAuthParamDTO;
-import com.ssafy.recodeli.user.model.dto.UserLoginParamDTO;
-import com.ssafy.recodeli.user.model.dto.UserLoginRespDTO;
-import com.ssafy.recodeli.user.model.dto.UserPasswordParamDTO;
-import com.ssafy.recodeli.user.model.dto.UserRegistParamDTO;
 import com.ssafy.recodeli.user.model.dto.UserSocialRegistParamDTO;
 import com.ssafy.recodeli.user.model.dto.UserUpdateParamDTO;
-import com.ssafy.recodeli.user.model.entity.User;
 import com.ssafy.recodeli.user.model.service.JwtService;
-import com.ssafy.recodeli.user.model.service.MailSendService;
 import com.ssafy.recodeli.user.model.service.UserService;
-import com.ssafy.recodeli.user.model.vo.ProviderType;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
@@ -51,47 +34,6 @@ public class UserRestController {
 	
 	private final UserService userService;
 	private final JwtService jwtService;
-	private final MailSendService mailService;
-	
-	/***
-	 * Email sending for Registration
-	 * @param UserRegistParamDTO
-	 * @return status 200, 409
-	 * @throws MessagingException 
-	 */
-//	@ApiOperation(value = "회원가입을 위한 이메일 전송", notes = "회원가입 정보를 기입받고 이메일을 전송합니다.")
-//	@PostMapping
-//	public ResponseEntity<Map<String, String>> TmpRegistration(
-//			@RequestBody @ApiParam(required = true) UserRegistParamDTO userDTO
-//			) throws MessagingException {
-//
-//		logger.info("Tmp Regist Info : {}", userDTO);
-//		Map<String, String> resultMap = new HashMap<String, String>();
-//
-//		String authKey = mailService.registMailSender(userDTO.getEmail());
-//		userService.tmpRegistration(userDTO, authKey);
-//		resultMap.put(RESULT, SUCCESS);
-//		return new ResponseEntity<Map<String, String>>(resultMap, HttpStatus.OK);
-//	}
-	
-	/***
-	 * User Registration
-	 * @param UserRegistParamDTO
-	 * @return status 200, 400, 409
-	 */
-//	@ApiOperation(value = "회원가입 이메일 인증", notes = "이메일 인증 후 회원가입을 진행합니다.")
-//	@PostMapping("/auth")
-//	public ResponseEntity<Map<String, String>> registration(
-//			@RequestBody @ApiParam(required = true) EmailAuthParamDTO authDTO
-//			) {
-//
-//		logger.info("Regist Info : {}", authDTO);
-//		Map<String, String> resultMap = new HashMap<String, String>();
-//
-//		userService.registEmailAuth(authDTO);
-//		resultMap.put(RESULT, SUCCESS);
-//		return new ResponseEntity<Map<String, String>>(resultMap, HttpStatus.OK);
-//	}
 	
 	/***
 	 * Social User Registration
@@ -101,7 +43,7 @@ public class UserRestController {
 	@ApiOperation(value = "소셜 회원가입", notes = "소셜 회원의 회원가입을 진행합니다.")
 	@PostMapping("/social")
 	public ResponseEntity<Map<String, String>> socialRegistration(
-			@RequestBody @ApiParam(required = true) UserSocialRegistParamDTO userDTO
+			@ApiParam(required = true) UserSocialRegistParamDTO userDTO
 			) {
 		
 		logger.info("Social Regist Info : {}", userDTO);
@@ -111,49 +53,7 @@ public class UserRestController {
 		resultMap.put(RESULT, SUCCESS);
 		return new ResponseEntity<Map<String, String>>(resultMap, HttpStatus.OK);
 	}
-	
-	/***
-	 * User Login
-	 * @param email
-	 * @param password
-	 * @return status 200, 401
-	 */
-//	@ApiOperation(value = "로그인", notes = "일반 로그인을 진행합니다.")
-//	@PostMapping("/login")
-//	public ResponseEntity<Map<String, Object>> generalLogin(
-//			@RequestBody @ApiParam(required = true) UserLoginParamDTO loginDTO
-//			) {
-//
-//		logger.info("login input parameter : {}", loginDTO);
-//		Map<String, Object> resultMap = new HashMap<String, Object>();
-//
-//		User user = userService.loginService(loginDTO);
-//		long userId = user.getUserId();
-//		String accessToken = jwtService.createAccessToken(userId);
-//		resultMap.put(RESULT, SUCCESS);
-//		resultMap.put("user", UserLoginRespDTO.of(user));
-//		resultMap.put("access-token", accessToken);
-//		return new ResponseEntity<Map<String, Object>>(resultMap, HttpStatus.OK);
-//	}
-	
-	/***
-	 * User email Duplicate Check
-	 * @param email
-	 * @return status 200, 409
-	 */
-//	@ApiOperation(value = "이메일 중복 확인", notes = "이메일이 중복되는 지 여부를 확인해줍니다.")
-//	@GetMapping("/email")
-//	public ResponseEntity<Map<String, String>> emailDuplicateCheck(
-//			@RequestParam(value = "email") @ApiParam(required = true) String email
-//			) {
-//
-//		logger.info("email duplicate check input parameter : {}", email);
-//		Map<String, String> resultMap = new HashMap<String, String>();
-//
-//		userService.emailDuplicateCheck(email);
-//		return new ResponseEntity<Map<String, String>>(resultMap, HttpStatus.OK);
-//	}
-	
+
 	/***
 	 * User nickname Duplicate Check
 	 * @param nickname
@@ -217,30 +117,6 @@ public class UserRestController {
 	}
 	
 	/***
-	 * User password Update
-	 * @param token
-	 * @param password
-	 * @param newPassword
-	 * @return status 200, 400, 401
-	 */
-//	@ApiOperation(value = "비밀번호 수정", notes = "해당 유저의 비밀번호를 수정합니다.")
-//	@PutMapping("/password")
-//	public ResponseEntity<Map<String, String>> updatePassword(
-//			@RequestHeader(value = "Authorization") @ApiParam(required = true) String token,
-//			@RequestBody @ApiParam(required = true) UserPasswordParamDTO passwordDTO
-//			) {
-//
-//		logger.info("updatePassword input parameter : {}", passwordDTO);
-//		Map<String, String> resultMap = new HashMap<String, String>();
-//
-//		jwtService.validateToken(token);
-//		long userId = jwtService.getUserId(token);
-//		userService.updatePassword(userId, passwordDTO.getPassword(), passwordDTO.getNewPassword());
-//		resultMap.put(RESULT, SUCCESS);
-//		return new ResponseEntity<Map<String, String>>(resultMap, HttpStatus.OK);
-//	}
-	
-	/***
 	 * User withdrawal
 	 * @param token
 	 * @return status 200, 401
@@ -261,35 +137,5 @@ public class UserRestController {
 		SecurityContextHolder.clearContext();
 		return new ResponseEntity<Map<String, String>>(resultMap, HttpStatus.OK);
 	}
-	
-	/***
-	 * Password lookup
-	 * @param token
-	 * @return 200, 401
-	 */
-//	@ApiOperation(value = "비밀번호 찾기", notes = "해당 유저의 비밀번호를 재설정하여 이메일로 송부합니다.")
-//	@GetMapping("/password")
-//	public ResponseEntity<Map<String, Object>> findPassword(
-//			@RequestParam @ApiParam(required = true) String email
-//			) {
-//
-//		logger.info("findPassword in");
-//		Map<String, Object> resultMap = new HashMap<String, Object>();
-//
-//		User user = userService.findUserByEmail(email);
-//
-//		if (user == null) {
-//			resultMap.put("result", "FAIL");
-//			resultMap.put("msg", "가입된 이메일이 아닙니다.");
-//			return new ResponseEntity<Map<String, Object>>(resultMap, HttpStatus.BAD_REQUEST);
-//		} else if (!user.getSocial().equals(ProviderType.O)) {
-//			resultMap.put("result", "FAIL");
-//			resultMap.put("msg", "소셜 회원입니다.");
-//			return new ResponseEntity<Map<String, Object>>(resultMap, HttpStatus.CONFLICT);
-//		}
-//		logger.info(email + "로 새로운 이메일을 송부합니다.");
-//		mailService.sendTmpPassword(user.getUserId(), user.getEmail());
-//		resultMap.put(RESULT, SUCCESS);
-//		return new ResponseEntity<Map<String, Object>>(resultMap, HttpStatus.OK);
-//	}
+
 }
