22424fae2035ca295fe7880fff0a8d09e6665c48
김진호 <wlsghsla94@naver.com>
2023-03-16 17:40:48 +0900

    doc: 네이버 웹소설 리뷰데이터 넷소설에서 크롤링해오기
    성은이가 크롤링해온 네이버 웹소설 데이터에서
    완결 200작품, 미완결 200작품을 무작위로 뽑아
    리뷰룰 긁어올 리소스 데이터 .csv 파일로 만듦
    
    해당 작품들을 순회하면서 100개 작품의 리뷰데이터가 쌓이면 크롤링 종료하는 것으로 계획

--- Crawling/naver_series_reviews.py
+++ Crawling/naver_series_reviews.py
@@ -1,37 +1,91 @@
 import csv
 import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as bs
 
 driver = webdriver.Chrome()
 
-ifilename = 'naver_novels_review'
+ifilename = 'naver_novel_reviews'
 ofilename = '{}_all'.format(ifilename)
 
 with open(ifilename+'.csv', 'r', encoding='utf-8') as csvfile:
     reader = csv.DictReader(csvfile)
     
-    fieldnames = reader.fieldnames + ['content'] + ['date'] + ['reviews_tags'] + ['spoiler_flag']
+    fieldnames = reader.fieldnames + ['content'] + ['rating'] + ['date'] + ['reviews_tags'] + ['spoiler_flag']
     
     with open(ofilename+'.csv', 'w', newline='', encoding='utf-8') as outfile:
         writer = csv.DictWriter(outfile, fieldnames=fieldnames)
         writer.writeheader()
+        url = 'https://sosul.network/'
+        driver.get(url)
         
         for row in reader:
-            # Construct the URL using the "Link" column of the current row
-            url = 'https://series.naver.com/' + row['link']
-
-            # Load the URL in the Chrome webdriver
-            driver.get(url)
+            Magnifier = driver.find_element(By.CLASS_NAME, "icon-search")
+            Magnifier.click()
             
-            # Extract the HTML content of the page
+            input_title = row['title']
+            search_input = driver.find_element(By.CLASS_NAME, "form-control")
+            search_input.send_keys(input_title)
+            search_input.send_keys(Keys.RETURN)
+            
             html = driver.page_source
+            soup = bs(html, "html.parser")
+            
+            if not soup.select('div', {'class': 'product'}):
+                continue
+            # 작품 클릭용
+            book_list = driver.find_elements(By.CLASS_NAME, "product-image")
+            # 작가 확인용
+            book_list_2 = soup.select("section > div > div> div >div.product")
+            # 검색해서 나온 작품들 순회
+            if book_list_2:
+                for b in range(len(book_list_2)):
+                    # 각 작품의 작가정보
+                    origins = book_list_2[b].find('div', {'class':'product-author'})
+                    # 해당 작품에 작가가 있고 내가 가지고 있는 작가 정보와 일치한다면
+                    if  origins and row['origin'] in origins.getText():
+                        # 해당 작품 상세페이지로 이동
+                        book_list[b].click()
+                        time.sleep(0.5)
+                        html = driver.page_source
+                        soup = bs(html, "html.parser")
+                        # 만약 리뷰가 있다면
+                        if soup.select('div.comment-list'):
+                            # 리뷰를 한 리스트에 담고
+                            reviews = soup.select('div.comment')
+                            print(len(reviews))
+                            # 담은 리스트를 순회함녀서 csv에 저장
+                            for rv in reviews:
+                                print(rv.text)
+                            break
+                        else:
+                            break
+            
+                # if b.find('div', {'class':'product-author'}) == row['origin']:
+                #     b.click()
+                #     break
+            
+            
+            
+            
+            
+            # print(row)
+            # Construct the URL using the "Link" column of the current row
+            # url = 'https://series.naver.com/' + row['link']
 
-            # Parse the HTML content using BeautifulSoup
-            soup = BeautifulSoup(html, 'html.parser')
+            # # Load the URL in the Chrome webdriver
+            # driver.get(url)
             
-            reviews = soup.select("ul.u_cbox_list")
-            time.sleep(1)
+            # # Extract the HTML content of the page
+            # html = driver.page_source
+
+            # # Parse the HTML content using BeautifulSoup
+            # soup = BeautifulSoup(html, 'html.parser')
             
-            print(reviews)
-            break
+            # reviews = soup.select("ul.u_cbox_list")
+            # time.sleep(1)
+            
+            # print(reviews)
+            # break
