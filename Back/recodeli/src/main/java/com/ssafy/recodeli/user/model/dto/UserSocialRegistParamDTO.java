d897aa1c6640b65545ab1cc5740e81f1b0e7ce89
전주영[서울_3반_A302]팀원 <wjswndud53@gmail.com>
2023-03-16 22:19:27 +0900

    feat: 임시저장

--- Back/recodeli/src/main/java/com/ssafy/recodeli/user/model/dto/UserSocialRegistParamDTO.java
+++ Back/recodeli/src/main/java/com/ssafy/recodeli/user/model/dto/UserSocialRegistParamDTO.java
@@ -10,6 +10,10 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.messaging.handler.annotation.MessageExceptionHandler;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
 
 @Builder
 @Getter
@@ -18,12 +22,23 @@ import lombok.ToString;
 @AllArgsConstructor
 @ToString
 public class UserSocialRegistParamDTO {
+
+	@NotNull(message = "email를 입력해주세요")
+	@Email
 	private String email;
+
+	@NotNull(message = "별명을 입력해주세요")
 	private String nickname;
+
+	@NotNull(message = "성별을 입력해주세요")
 	private String gender;
+
+	@NotNull(message = "나이대를 입력해주세요")
 	private String age;
+
+	@NotNull(message = "소셜종류가 null입니다")
 	private String social;
-	
+
 	public static User of(UserSocialRegistParamDTO userDTO) {
 		String receivedSocial = userDTO.getSocial().toUpperCase();
 		if (receivedSocial.equals("K")) {
