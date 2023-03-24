77855b8716e363a6dc0d8c179b40b23b150ad05b
김진호 <wlsghsla94@naver.com>
2023-03-24 17:57:02 +0900

    feat: toonbti spring 프로젝트 생성

---
+++ Back/toonbti/src/main/java/com/ssafy/toonbti/ToonbtiApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.toonbti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToonbtiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToonbtiApplication.class, args);
+	}
+
+}
