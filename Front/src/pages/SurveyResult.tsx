bc12bf7f49ae376cdca5df15d911b90402a539ee
isabel-noh <nohhyunjeong93@gmail.com>
2023-03-27 14:11:10 +0900

    result page layout

--- Front/src/pages/SurveyResult.tsx
+++ Front/src/pages/SurveyResult.tsx
@@ -1,6 +1,5 @@
 import { Button } from "antd";
 import React from "react";
-import { Bar } from "react-chartjs-2";
 import styled from "styled-components";
 import { Layout, ProgressiveBar } from "../components/common";
 import Image from "../components/common/Image";
@@ -14,10 +13,10 @@ import {
   LinearScale,
 } from "chart.js";
 import { useNavigate } from "react-router-dom";
+import tiger from "/tiger.jpg";
 
-ChartJS.register(BarElement, Tooltip, CategoryScale, LinearScale);
 //TODO: data fetch
-
+const data = {};
 export default function AnalysisResult() {
   const navigate = useNavigate();
 
@@ -95,8 +94,7 @@ export default function AnalysisResult() {
           <Text>내가 지금까지 읽은 웹툰의 수는?</Text>
           <CallOutDiv>
             <Text size="1.5rem">
-              <span style={{ color: "#1890ff", fontWeight: "700" }}>77</span>개
-              {/* data */}
+              <PointSpan>77</PointSpan>개{/* data */}
             </Text>
           </CallOutDiv>
           <Text>제법 많이 보셨군요!</Text> {/* 읽은 권수에 따른 다른 text */}
@@ -107,10 +105,10 @@ export default function AnalysisResult() {
             <PointSpan>N명</PointSpan>의 분석 독자들 중...
           </Text>
           <div style={{ width: "90%", margin: "auto" }}>
-            <ProgressiveBar progress={50}></ProgressiveBar>
+            <ProgressiveBar type="top" progress={90}></ProgressiveBar>
           </div>
           <Text>
-            <span>상위 NN.N%</span>에 해당해요!
+            <GradientText>상위 NN.N%</GradientText>에 해당해요!
           </Text>
         </StyledSection>
         <StyledSection>
@@ -126,7 +124,7 @@ export default function AnalysisResult() {
                 카카오 매니아
               </Text>
             </RoundDiv>
-            <RoundDiv color="#4a4b4c">
+            <RoundDiv color="#5a5b5c">
               <Text color="#ffffff" size="1.2rem" bold>
                 완결작 킬러
               </Text>
@@ -136,28 +134,114 @@ export default function AnalysisResult() {
         <StyledSection>
           <Text size="1.3rem">플랫폼 비율</Text>
           <div>
-            <Bar data={data} options={options} />
+            <RatioTextBox space>
+              <RatioText color="#FFBC00">카카오페이지 NN%</RatioText>
+              <RatioText color="#2DB400">네이버 NN%</RatioText>
+            </RatioTextBox>
+            <ProgressiveBar type="platform" progress={73}></ProgressiveBar>
+            <RatioTextBox>
+              {/* {data.kakao > data.naver ? (
+                <RatioText color="#FFBC00">카카오페이지</RatioText>
+              ) : (
+                <RatioText color="#2DB400">네이버</RatioText>
+              )} */}
+              <RatioText color="#FFBC00">카카오페이지</RatioText>
+              <RatioText>의 웹툰을 더 많이 읽고 있어요.</RatioText>
+            </RatioTextBox>
           </div>
         </StyledSection>
         <StyledSection>
           <Text size="1.3rem">완결작 비율</Text>
-          <div></div>
+          <div>
+            <RatioTextBox space>
+              <RatioText color="#FF6C6C">완결작 NN%</RatioText>
+              <RatioText color="#1E9EFF">연재작 NN%</RatioText>
+            </RatioTextBox>
+            <ProgressiveBar type="endedOrOngoin" progress={43}></ProgressiveBar>
+
+            <RatioTextBox>
+              <RatioText color="#1E9EFF">연재작 </RatioText>
+              <RatioText>보다 더&nbsp;</RatioText>
+              <RatioText color="#FF6C6C"> 완결작</RatioText>
+              <RatioText>을 더 선호해요.</RatioText>
+            </RatioTextBox>
+          </div>
         </StyledSection>
         <StyledSection>
           <Text size="1.3rem">사용자가 많이 본 장르</Text>
           <div>
-            <div>육각형 그래프</div>
-            <div>장르 성분표</div>
-            <Text>
-              주로 <span>&quot;로맨스 판타지, 판타지, 일상/개그&quot;</span>
-              장르를 더 선호하시네요!
-            </Text>
-            {/* data */}
+            <section className="genre_graph">육각형 그래프</section>
+            <section className="genre_table">
+              <GenreTableTitleDiv>
+                <GenreTableTitle>장르 성분표</GenreTableTitle>
+              </GenreTableTitleDiv>
+              <GenreSect>
+                <GenreDiv>
+                  <GenreText preferred>#판타지</GenreText>
+                  <GenreHr preferred />
+                  <GenreText preferred>38 (13.9%)</GenreText>
+                </GenreDiv>
+                <GenreDiv>
+                  <GenreText>#드라마</GenreText>
+                  <GenreHr />
+                  <GenreText>21 (7.7%)</GenreText>
+                </GenreDiv>
+                <GenreDiv>
+                  <GenreText>#로맨스</GenreText>
+                  <GenreHr />
+                  <GenreText>10 (3.6%)</GenreText>
+                </GenreDiv>
+                <GenreDiv>
+                  <GenreText preferred>#판타지</GenreText>
+                  <GenreHr preferred />
+                  <GenreText preferred>38 (13.9%)</GenreText>
+                </GenreDiv>
+                <GenreDiv>
+                  <GenreText>#드라마</GenreText>
+                  <GenreHr />
+                  <GenreText>21 (7.7%)</GenreText>
+                </GenreDiv>
+                <GenreDiv>
+                  <GenreText preferred>#로맨스</GenreText>
+                  <GenreHr preferred />
+                  <GenreText preferred>10 (3.6%)</GenreText>
+                </GenreDiv>
+                <GenreDiv>
+                  <GenreText preferred>#판타지</GenreText>
+                  <GenreHr preferred />
+                  <GenreText preferred>38 (13.9%)</GenreText>
+                </GenreDiv>
+                <GenreDiv>
+                  <GenreText>#드라마</GenreText>
+                  <GenreHr />
+                  <GenreText>21 (7.7%)</GenreText>
+                </GenreDiv>
+                <GenreDiv>
+                  <GenreText>#로맨스</GenreText>
+                  <GenreHr />
+                  <GenreText>10 (3.6%)</GenreText>
+                </GenreDiv>
+                <GenreDiv>
+                  <GenreText>#로맨스</GenreText>
+                  <GenreHr />
+                  <GenreText>10 (3.6%)</GenreText>
+                </GenreDiv>
+              </GenreSect>
+            </section>
+            <RatioTextBox>
+              <Text bold size="1.1rem">
+                주로
+                <span style={{ color: "#FF6C6C" }}>
+                  로맨스 판타지, 판타지, 일상/개그
+                </span>
+                장르를 <br />더 선호하시네요!
+              </Text>
+            </RatioTextBox>
           </div>
         </StyledSection>
         <StyledSection>
-          <Text>
-            <b>&quot;로맨스판타지&quot;</b> 장르를 좋아하는 독자들의 선택
+          <Text size="1.1rem">
+            <BoldSpan>로맨스판타지</BoldSpan> 장르를 좋아하는 독자들의 선택
           </Text>
           <section>
             <RecommendItemList
@@ -174,20 +258,28 @@ export default function AnalysisResult() {
         </StyledSection>
         <StyledSection>
           <Text>사용자가 즐겨보는 키워드</Text>
-          <section style={{ display: "flex" }}>
-            <div>
-              <Text>#&quot;잔잔한&quot;</Text>
-            </div>
-            <div>
-              <Text>#&quot;힐링&quot;</Text>
-            </div>
-            <div>
-              <Text>#&quot;일상&quot;</Text>
-            </div>
-            <div>
-              <Text>#&quot;호랑이&quot;</Text>
-            </div>
-          </section>
+          <KeywordSection>
+            <KeywordDiv>
+              <Text type="keyword" bold>
+                #잔잔한
+              </Text>
+            </KeywordDiv>
+            <KeywordDiv>
+              <Text type="keyword" bold>
+                #힐링
+              </Text>
+            </KeywordDiv>
+            <KeywordDiv>
+              <Text type="keyword" bold>
+                #일상
+              </Text>
+            </KeywordDiv>
+            <KeywordDiv>
+              <Text type="keyword" bold>
+                #호랑이
+              </Text>
+            </KeywordDiv>
+          </KeywordSection>
         </StyledSection>
         <StyledSection>
           <RecommendItemList
@@ -196,22 +288,55 @@ export default function AnalysisResult() {
           ></RecommendItemList>
         </StyledSection>
         <StyledSection>
-          <Text>
-            &quot;로맨스 판타지&quot; 장르 독자들이 선호하는 대표 작가
+          <Text bold>로맨스 판타지 장르 독자들이 선호하는 대표 작가</Text>
+          <Image url="" width="10rem" height="14rem" borderRadius={4}></Image>
+          <Text bold>
+            <PointSpan>취향저격율 89.4%</PointSpan>
           </Text>
-          <Image url="" width="14rem" height="14rem" borderRadius={4}></Image>
           <Text>
-            <span>취향저격율 &quot;NN.N%&quot;</span>
+            <BoldSpan>박태준주식회사 / 송펄 / 마이노 </BoldSpan>
+            작가
           </Text>
-          <Text>&quot;작가1 / 작가2 / 작가3&quot; 작가</Text>
-          <Text>대표작 - &quot;작품1, 작품2&quot;</Text>
-          <Text>주요 연재 장르 - &quot;장르1, 장르2&quot;</Text>
+          <Text>대표작 - 작품1, 작품2</Text>
+          <Text>주요 연재 장르 - 장르1, 장르2</Text>
         </StyledSection>
         <StyledSection>
-          <Text>&quot;로맨스 판타지&quot; 장르 독자들이 선호하는 작가들</Text>
-          <div>작가1</div>
-          <div>작가2</div>
-          <div>작가3</div>
+          <Text bold>로맨스 판타지 장르 독 자들이 선호하는 작가들</Text>
+          <section>
+            <AuthorsDiv>
+              <Image url={tiger} width="6rem" height="8rem" />
+              <DescAuthorDiv>
+                <Text>
+                  <BoldSpan>삼</BoldSpan> 작가
+                </Text>
+                <Text>
+                  대표작 - <span>김부장</span>
+                </Text>
+              </DescAuthorDiv>
+            </AuthorsDiv>
+            <AuthorsDiv>
+              <Image url={tiger} width="6rem" height="8rem" />
+              <DescAuthorDiv>
+                <Text>
+                  <BoldSpan>삼</BoldSpan> 작가
+                </Text>
+                <Text>
+                  대표작 - <span>신의 탑</span>
+                </Text>
+              </DescAuthorDiv>
+            </AuthorsDiv>
+            <AuthorsDiv>
+              <Image url={tiger} width="6rem" height="8rem" />
+              <DescAuthorDiv>
+                <Text>
+                  <BoldSpan>삼</BoldSpan> 작가
+                </Text>
+                <Text>
+                  대표작 - <span>전지적 독자 시점</span>
+                </Text>
+              </DescAuthorDiv>
+            </AuthorsDiv>
+          </section>
         </StyledSection>
         <StyledButton
           onClick={() => {
@@ -252,17 +377,109 @@ const RoundUpperDiv = styled.div`
   /* display: flex;
   justify-content: space-evenly; */
   display: flex;
-  grid-template-columns: 1fr 1fr 1fr;
-  height: 108px;
+  /* grid-template-columns: 1fr 1fr 1fr; */
+  grid-template-columns: repeat(auto-fit, minmax(100px, 1fr));
+  gap: 10px;
+  width: 100%;
+  justify-content: center;
 `;
 const RoundDiv = styled.div<{ color?: string }>`
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 100%;
-  border-radius: 100px;
+  border-radius: 50%;
+  width: 80px;
+  height: 80px;
   background-color: ${(props) => props.color};
   padding: 0.5rem;
   word-break: keep-all;
   margin: 0.25rem;
+  @media (min-width: 320px) and (max-width: 380px) {
+    width: 80px;
+    height: 80px;
+  }
+  @media (min-width: 380px) and (max-width: 390px) {
+    width: 100px;
+    height: 100px;
+  }
+  @media (min-width: 391px) {
+    width: 120px;
+    height: 120px;
+  }
+`;
+
+const RatioTextBox = styled.div<{ space?: boolean }>`
+  display: flex;
+  justify-content: ${(props) => (props.space ? "space-between" : "center")};
+  font-size: 1rem;
+`;
+
+const RatioText = styled.p<{ color?: string }>`
+  color: ${(props) => (props.color ? props.color : "black")};
+  font-weight: 700;
+  display: flex;
+`;
+const GradientText = styled.span`
+  margin-bottom: 0.5rem;
+  font-size: 1.5rem;
+  font-weight: 700;
+  display: block;
+  background: linear-gradient(#72e5ec 0%, #a7ffba 100%);
+  background-clip: text;
+  -webkit-background-clip: text;
+  color: transparent;
+`;
+const GenreText = styled.p<{ preferred?: boolean }>`
+  color: ${(props) => (props.preferred ? "#FF6C6C" : "black")};
+  margin: auto 0.5rem;
+`;
+const GenreHr = styled.hr<{ preferred?: boolean }>`
+  background: ${(props) => (props.preferred ? "#FF6C6C" : "black")};
+  border: 1px;
+  height: 1px;
+  flex-grow: 1;
+`;
+const GenreDiv = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+`;
+const GenreSect = styled.section`
+  background-color: #eeeeee;
+  padding: 0.75rem;
+`;
+const GenreTableTitleDiv = styled.div`
+  border-top: 1.5px solid black;
+  border-bottom: 1.5px solid black;
+  padding: 0.25rem 0 0.25rem 1rem;
+`;
+const GenreTableTitle = styled.h4`
+  margin: 0;
+  text-align: start;
+`;
+const KeywordDiv = styled.div`
+  border-radius: 4px;
+  background-color: #eeeeee;
+  width: fit-content;
+`;
+const KeywordSection = styled.section`
+  justify-content: center;
+  gap: 0.5rem;
+  display: flex;
+`;
+const AuthorsDiv = styled.div`
+  width: 100%;
+  padding: 0.5rem 1rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 0.5rem;
+  background-color: #eeeeee;
+  border-radius: 8px;
+`;
+const DescAuthorDiv = styled.div`
+  width: 60%;
+`;
+const BoldSpan = styled.span`
+  font-weight: 700;
 `;
