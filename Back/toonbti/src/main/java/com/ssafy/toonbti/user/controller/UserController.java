fd97d1251861270d367cfc1811a093156fd64866
권성은 <isungun@naver.com>
2023-04-04 12:04:06 +0900

    Update Back/toonbti/src/main/java/com/ssafy/toonbti/user/controller/UserController.java

--- Back/toonbti/src/main/java/com/ssafy/toonbti/user/controller/UserController.java
+++ Back/toonbti/src/main/java/com/ssafy/toonbti/user/controller/UserController.java
@@ -13,6 +13,7 @@ import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import javax.validation.Valid;
 import java.util.Arrays;
