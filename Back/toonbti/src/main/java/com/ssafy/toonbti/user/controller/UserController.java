9f3ebcda2da08deebd025478b8f58147052ff86d
전주영[서울_3반_A302]팀원 <wjswndud53@gmail.com>
2023-04-04 10:30:07 +0900

    Resolve "spring boot 로그 추가"

--- Back/toonbti/src/main/java/com/ssafy/toonbti/user/controller/UserController.java
+++ Back/toonbti/src/main/java/com/ssafy/toonbti/user/controller/UserController.java
@@ -1,11 +1,14 @@
 package com.ssafy.toonbti.user.controller;
 
+import com.ssafy.toonbti.user.model.dto.TypeAnswerDTO;
 import com.ssafy.toonbti.user.model.dto.UserAnswerDTO;
 import com.ssafy.toonbti.user.model.dto.UserDTO;
 import com.ssafy.toonbti.user.model.dto.UserResultDTO;
 import com.ssafy.toonbti.user.model.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
@@ -20,7 +23,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
-//    logger.info("Social Regist Info : {}", user);
+    private final Logger logger = LoggerFactory.getLogger(UserController.class);
 
     /***
      * 모든 유저 수 반환
@@ -28,6 +31,7 @@ public class UserController {
      */
     @QueryMapping
     public long countAllUsers(){
+        logger.info("countAllUsers");
         return userService.getUserCounts();
     }
 
@@ -36,7 +40,8 @@ public class UserController {
      * @param input
      */
     @MutationMapping
-    public UserResultDTO addUserResponse(@Argument UserAnswerDTO input) {
+    public UserResultDTO addUserResponse(@Argument UserAnswerDTO input){
+        logger.info("addUserResponse | input: {}", input);
         return userService.addUserResponse(input);
     }
 
@@ -46,6 +51,14 @@ public class UserController {
      */
     @QueryMapping
     public List<UserDTO> getAllTypes() {
+        logger.info("getAllTypes");
         return userService.getAllTypes();
     }
+
+    @QueryMapping
+    public TypeAnswerDTO getType(@Argument String userType) {
+        logger.info("getType | userType : {}", userType);
+        return userService.getByType(userType);
+    }
+
 }
--- Back/toonbti/src/main/java/com/ssafy/toonbti/webtoon/controller/WebtoonController.java
+++ Back/toonbti/src/main/java/com/ssafy/toonbti/webtoon/controller/WebtoonController.java
@@ -4,9 +4,10 @@ import com.ssafy.toonbti.webtoon.model.dto.WebtoonResultDTO;
 import com.ssafy.toonbti.webtoon.model.service.WebtoonService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
-import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -14,9 +15,11 @@ import org.springframework.stereotype.Controller;
 @RequiredArgsConstructor
 public class WebtoonController {
     private final WebtoonService webtoonService;
+    private final Logger logger = LoggerFactory.getLogger(WebtoonController.class);
 
     @MutationMapping
     public WebtoonResultDTO createResult(@Argument Long userId){
+        logger.info("createResult | userId: {}", userId);
         return webtoonService.createResult(userId);
     }
 
