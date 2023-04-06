59ffc0a915a5db9c6e787653f8cb98bf495193f6
김진호 <wlsghsla94@naver.com>
2023-04-06 09:26:05 +0900

    Resolve "django exception handling"

--- Back/Django/toonbti/settings.py
+++ Back/Django/toonbti/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-i*=od-y2#wo41kkrt+a)g_5+^_(7ku)&+zjcec3-4l26$%!s(y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
