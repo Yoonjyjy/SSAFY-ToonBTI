dcca6bf1ed35289fb15090fa7e3434356f09a46e
전주영[서울_3반_A302]팀원 <wjswndud53@gmail.com>
2023-04-06 15:56:38 +0900

    feat:랜덤 이미지 12개

--- Back/toonbti/src/main/java/com/ssafy/toonbti/webtoon/model/repository/WebtoonRepository.java
+++ Back/toonbti/src/main/java/com/ssafy/toonbti/webtoon/model/repository/WebtoonRepository.java
@@ -2,8 +2,13 @@ package com.ssafy.toonbti.webtoon.model.repository;
 
 import com.ssafy.toonbti.webtoon.model.entity.Webtoon;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface WebtoonRepository extends JpaRepository<Webtoon, Long> {
+    @Query(value = "select image from webtoon order by rand() limit 12", nativeQuery = true)
+    List<String> getRand12();
 }
